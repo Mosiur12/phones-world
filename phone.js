9c641c8fc1d7df06d550ee7b2649d5032ffc77cc
Mosiur12 <mrsojib9600@gmail.com>
2022-03-02 01:17:43 +0600

    phone add

--- phone.js
+++ phone.js
@@ -4,4 +4,18 @@ const searchPhone = () =>{
     console.log(searchText);
 
     searchField.value = "";
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+
+    fetch(url)
+    .then (res => res.json())
+    .then (data => displaySearch(data.data));
+}
+
+const displaySearch = data =>{
+    const searchResult = document.getElementById('search-result');
+    data.forEach(phone => {
+        console.log(phone);
+        
+    });
 }
