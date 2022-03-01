6648d9102529effbf80d190329b62bfe704e18d8
Mosiur12 <mrsojib9600@gmail.com>
2022-03-02 01:54:31 +0600

    new feature

--- phone.js
+++ phone.js
@@ -16,6 +16,17 @@ const displaySearch = data =>{
     const searchResult = document.getElementById('search-result');
     data.forEach(phone => {
         console.log(phone);
-        
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>`;
+
+        searchResult.appendChild(div);
     });
 }
