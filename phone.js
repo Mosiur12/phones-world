57a61103506b20fcc856624a22e688dbcac03bd4
Mosiur12 <mrsojib9600@gmail.com>
2022-03-02 11:58:09 +0600

    new added

--- phone.js
+++ phone.js
@@ -5,7 +5,7 @@ noFound.style.display = 'none';
 const searchPhone = () =>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value ;
-    // console.log(searchText);
+    
 // -------clear data
     searchField.value = "";
 
@@ -25,19 +25,34 @@ const displaySearch = data =>{
     }
 
     data.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100">
+        <div class="card h-100 shadow-lg p-3 mb-5 bg-body rounded">
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <p class="card-text">${phone.brand}</p>
-          <button type="button" class="btn btn-primary">Details</button>
+          <button onclick="details('${phone.slug}')" type="button" class="btn btn-primary">Details</button>
         </div>
       </div>`;
 
         searchResult.appendChild(div);
     });
+
+
 }
+
+const details = (detail) => {
+  // console.log(detail);
+  const url = `https://openapi.programming-hero.com/api/phone/${detail}`;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => detailInfo(data.data));
+  };
+
+  const detailInfo = (info) => {
+    console.log(info);
+
+  }
